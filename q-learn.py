08d3416e5a2ccb71dad5055e56d860302e8e75af
Baptiste Moureaux <baptiste.moureaux@loungeup.com>
2023-03-31 11:37:10 +0200

    add working Q learning

--- q-learn.py
+++ q-learn.py
@@ -1,100 +1,106 @@
-from IPython.display import clear_output
-import numpy as np
-import random
 import gymnasium as gym
-import time
+import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-environment = gym.make("FrozenLake-v1", is_slippery=True)
+environment = gym.make("FrozenLake-v1")
+# environment = gym.make("FrozenLake-v1", render_mode="human", is_slippery=True)
 environment.reset()
-environment.render()
-
-# Our table has the following dimensions:
-# (rows x columns) = (states x actions) = (16 x 4)
-qtable = np.zeros((16, 4))
-
-# Alternatively, the gym library can also directly g
-# give us the number of states and actions using
-# "env.observation_space.n" and "env.action_space.n"
-nb_states = environment.observation_space.n  # = 16
-nb_actions = environment.action_space.n  # = 4
-qtable = np.zeros((nb_states, nb_actions))
-
-# Let's see how it looks
-print("Q-table =")
-print(qtable)
-
-action = environment.action_space.sample()
-
-# 2. Implement this action and move the agent in the desired direction
-new_state, reward, terminated, info, truncated = environment.step(action)
-
-# Display the results (reward and map)
-environment.render()
-print(f"Reward = {reward}")
-
-plt.rcParams["figure.dpi"] = 300
-plt.rcParams.update({"font.size": 17})
 
 # We re-initialize the Q-table
 qtable = np.zeros((environment.observation_space.n, environment.action_space.n))
 
 # Hyperparameters
-episodes = 1000  # Total number of episodes
-alpha = 0.5  # Learning rate
-gamma = 0.9  # Discount factor
+episodes = 30000        # Total number of episodes
+alpha = 0.9        # Learning rate
+gamma = 0.9            # Discount factor
+epsilon = 1.0          # Amount of randomness in the action selection
+epsilon_decay = 0.001  # Fixed amount to decrease
 
 # List of outcomes to plot
 outcomes = []
 
-print("Q-table before training:")
-print(qtable)
-
 # Training
-for _ in range(episodes):
-    state = environment.reset()[0]
-    terminated = False
+start_time = time.time() # Start time of training
+for i in range(episodes):
+    state = environment.reset()
+    done = False
 
     # By default, we consider our outcome to be a failure
     outcomes.append("Failure")
 
     # Until the agent gets stuck in a hole or reaches the goal, keep training it
-    while not terminated:
-        # Choose the action with the highest value in the current state
-        print(int(np.max(qtable[0])))
-        if np.max(int(qtable[state][0])) > 0:
-          print("popo")
-          action = np.argmax(int(qtable[state[0]]))
+    while not done:
+        # Generate a random number between 0 and 1
+        rnd = np.random.random()
 
-        # If there's no best action (only zeros), take a random one
+        # If random number < epsilon, take a random action
+        if rnd < epsilon:
+            action = environment.action_space.sample()
+        # Else, take the action with the highest value in the current state
         else:
-          action = environment.action_space.sample()
-             
+            action = np.argmax(qtable[state[0]])
+
         # Implement this action and move the agent in the desired direction
-        onservation, reward, terminated, truncated, info = environment.step(action)
+        observation, reward, terminated, truncated, info = environment.step(action)
 
         # Update Q(s,a)
-        print(state)
-        qtable[int(state), action] = qtable[state, action] + \
-                                alpha * (reward + gamma * np.max(qtable[new_state]) - qtable[state, action])
-        
+        qtable[state[0], action] = qtable[state[0], action] + \
+            alpha * (reward + gamma *
+                     np.max(qtable[observation]) - qtable[state[0], action])
+
         # Update our current state
-        state = onservation
-        
+        state = (observation,)
+
         # If we have a reward, it means that our outcome is a success
         if reward:
-          outcomes[-1] = "Success"
+            outcomes[-1] = "Success"
+            
+        done = terminated or truncated
 
-print()
-print('===========================================')
-print('Q-table after training:')
-print(qtable)
-print("aled")
-# Plot outcomes
-plt.figure(figsize=(12, 5))
-plt.xlabel("Run number")
-plt.ylabel("Outcome")
-ax = plt.gca()
-ax.set_facecolor('#efeeea')
-plt.bar(range(len(outcomes)), outcomes, color="#0A047A", width=1.0)
-plt.show()
+    # Update epsilon
+    epsilon = max(epsilon - epsilon_decay, 0)
+
+    # Calculate and print progress
+    if i % 1000 == 0:
+        print(f"Training progress: {i/episodes*100:.2f}%", end="\r")
+
+training_time = time.time() - start_time # Total training time
+print(f"Training time: {training_time:.2f} seconds")
+
+# Evaluation
+start_time = time.time() # Start time of evaluation
+episodes = 1000
+nb_success = 0
+
+
+environment = gym.make("FrozenLake-v1", render_mode="human")
+# environment = gym.make("FrozenLake-v1", render_mode="human", is_slippery=True)
+
+for i in range(episodes):
+    state = environment.reset()
+    terminated = False
+
+    # Until the agent gets stuck or reaches the goal, keep training it
+    while not terminated:
+        # Choose the action with the highest value in the current state
+        action = np.argmax(qtable[state[0]])
+
+        # Implement this action and move the agent in the desired direction
+        observation, reward, terminated, truncated, info = environment.step(action)
+
+        # Update our current state
+        state = (observation,)
+
+        # When we get a reward, it means we solved the game
+        nb_success += reward
+
+    # Calculate and print progress
+    if i % 100 == 0:
+        print(f"Evaluation progress: {i/episodes*100:.2f}%", end="\r")
+
+evaluation_time = time.time() - start_time # Total training time
+print(f"Evaluation time: {evaluation_time:.2f} seconds")
+print(f"Success rate = {nb_success/episodes*100}%")
+
+#? Pourquoi le success rate n'est pas de 100% en non slippery mdoe ?
